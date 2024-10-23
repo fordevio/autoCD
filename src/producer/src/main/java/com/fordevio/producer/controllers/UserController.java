ec64e25987c1542ebf6bdeea9056976d5f79a12b
YASH PATEL <yp969803@gmail.com>
2024-10-23 16:13:23 +0530

    feat: update user

--- src/producer/src/main/java/com/fordevio/producer/controllers/UserController.java
+++ src/producer/src/main/java/com/fordevio/producer/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -88,6 +89,28 @@ public class UserController {
            return ResponseEntity.internalServerError().body(new MessageResponse(e.getMessage()));
        }
           
+     }
+
+     @PutMapping("/admin/update/{id}")
+     public ResponseEntity<?> updateUser(@PathVariable Long id, @Valid @RequestBody AddUpdateUserRequest user){
+         try{
+             User userToUpdate = userHandler.getUserById(id);
+             if(userToUpdate == null){
+                 return ResponseEntity.badRequest().body(new MessageResponse("User not found"));
+             }
+             User adminUser = userHandler.getAdminUser();
+             Set<Permission> permissions = userToUpdate.getPermissions();
+             if (adminUser!= null&& user.getPermissions() != null && !adminUser.getId().equals(id)){
+                 permissions = user.getPermissions();
+             }
+             userToUpdate.setUsername(user.getUsername());
+             userToUpdate.setPassword(user.getPassword());
+             userToUpdate.setPermissions(permissions);
+             userHandler.saveUser(userToUpdate);
+             return ResponseEntity.ok(userToUpdate);
+         }catch(Exception e){
+             return ResponseEntity.internalServerError().body(new MessageResponse(e.getMessage()));
+         }
      }
 
 }
