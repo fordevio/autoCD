837e1b81ff43e418a2d56db5570ec17b95892551
YASH PATEL <yp969803@gmail.com>
2024-12-04 22:01:08 +0530

    feat: added endpoint for project cd

--- src/producer/src/main/java/com/fordevio/producer/controllers/KafkaController.java
+++ src/producer/src/main/java/com/fordevio/producer/controllers/KafkaController.java
@@ -1,11 +1,19 @@
 package com.fordevio.producer.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fordevio.producer.models.Project;
+import com.fordevio.producer.payloads.response.MessageResponse;
+import com.fordevio.producer.services.ProjectHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/deliver")
 public class KafkaController {
@@ -13,15 +21,25 @@ public class KafkaController {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
  
+    @Autowired
+    private ProjectHandler projectHandler;
+
     public void sendMessage(String topicName,String msg) {
       kafkaTemplate.send(topicName, msg);
     }   
     
     @PostMapping("/{projectName}")
-    public void deliverProject(String projectName){
-        sendMessage(projectName, projectName);
+    public ResponseEntity<?> deliverProject(String projectName){
+        try{
+            Project project = projectHandler.getProjectByName(projectName);
+            if(project == null){
+                return ResponseEntity.badRequest().body(new MessageResponse("Project does not exist"));
+            }
+            sendMessage(projectName, "CD request to deliver project: "+projectName);
+            return ResponseEntity.ok(new MessageResponse("Project delivered successfully"));
+        }catch(Exception e){
+            log.warn("Error while getting project logs", e);
+            return ResponseEntity.internalServerError().body(new MessageResponse(e.getMessage()));
+        }
     }
-
-    
-
 }
