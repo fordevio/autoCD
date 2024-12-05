7eb124c3f610b5e1be243c90a167bbda863a56cd
YASH PATEL <yp969803@gmail.com>
2024-12-05 14:16:08 +0530

    rfac: added env variable for kafka host

--- src/producer/src/main/java/com/fordevio/producer/services/kafka/KafkaProducerConfig.java
+++ src/producer/src/main/java/com/fordevio/producer/services/kafka/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -13,10 +14,14 @@ import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaProducerConfig {
+
+   @Value("${kafka.host}")
+   private String host;
+
    @Bean
    ProducerFactory<String, String> producerFactory() {
       Map<String, Object> configProps = new HashMap<>();
-      configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+      configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host);
       configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
       configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
       return new DefaultKafkaProducerFactory<>(configProps);
