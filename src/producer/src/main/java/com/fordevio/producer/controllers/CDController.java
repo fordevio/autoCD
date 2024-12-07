60994044dc40f5a800fc780c04aedbf82205efcd
YASH PATEL <yp969803@gmail.com>
2024-12-07 22:51:56 +0530

    rfac: delivery endpoint

--- src/producer/src/main/java/com/fordevio/producer/controllers/CDController.java
+++ src/producer/src/main/java/com/fordevio/producer/controllers/CDController.java
@@ -1,8 +1,11 @@
 package com.fordevio.producer.controllers;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fordevio.producer.models.Project;
 import com.fordevio.producer.payloads.response.MessageResponse;
+import com.fordevio.producer.services.FileHandlerSvc;
 import com.fordevio.producer.services.ProjectHandler;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,26 +22,35 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/protected/deliver")
 public class CDController {
-    
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
  
+    @Autowired
+    private FileHandlerSvc fileHandlerSvc;
+
     @Autowired
     private ProjectHandler projectHandler;
 
-    public void sendMessage(String key,String msg) {
-      kafkaTemplate.send("autocd",key, msg);
-    }   
     
     @PostMapping("/{projectName}")
     public ResponseEntity<?> deliverProject(@PathVariable String projectName){
+
         try{
             Project project = projectHandler.getProjectByName(projectName);
             if(project == null){
                 return ResponseEntity.badRequest().body(new MessageResponse("Project does not exist"));
             }
             String scriptFilePath = "/var/autocd/scripts/"+projectName+".sh";
-             String logFilePath = "/var/autocd/logs/"+projectName+".log";
+            String logFilePath = "/var/autocd/logs/"+projectName+".log";
+            ExecutorService executorService = Executors.newFixedThreadPool(7);
+            executorService.submit(() -> {
+                try { 
+                    fileHandlerSvc.executeShellScript(scriptFilePath, logFilePath);
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+    
+            executorService.shutdown();
+
             log.info("Delivered CD msg for: {}", projectName);
             return ResponseEntity.ok(new MessageResponse("Project CD successfully"));
         }catch(Exception e){
