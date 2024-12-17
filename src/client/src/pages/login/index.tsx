61bc600fffc515fbebd87846598a57e95417ae34
YASH PATEL <yp969803@gmail.com>
2024-12-17 19:37:39 +0530

    feat: added inputs in login page

--- src/client/src/pages/login/index.tsx
+++ src/client/src/pages/login/index.tsx
@@ -1,7 +1,22 @@
+import "./index.css"
+import logo from '../../assets/autocd-logo.png'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+     <div className="log-div">
+     <img className="logo" src={logo}/>
+     <div className="input-div">
+        <div className="input">
+            <span className="input-box">Username: </span><input type="text" />
+        </div>
+        <div className="input">
+            <span className="input-box">Password: </span><input type="password" />
+        </div>
+        <button>Login</button>
+     </div>
+     </div>
+    </>
   )
 }
 
