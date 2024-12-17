80cba4f90987dddb364dd8782d71a4d71a7b8adf
YASH PATEL <yp969803@gmail.com>
2024-12-17 23:59:12 +0530

    rfac: cors config

--- src/producer/src/main/java/com/fordevio/producer/services/security/SecurityConfig.java
+++ src/producer/src/main/java/com/fordevio/producer/services/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors();
+   
         http.csrf(csrf -> csrf.disable()).exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler)).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth.requestMatchers("/api/protected/**").authenticated().anyRequest().permitAll());
         http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
